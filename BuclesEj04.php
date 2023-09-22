b34694058ed8e7496da012f9d582d148cd9e8fe9
Franyxy <117320622+Franyxy@users.noreply.github.com>
2023-09-22 10:24:00 +0200

    Create BuclesEj04.php

---
+++ BuclesEj04.php
@@ -0,0 +1,12 @@
+<HTML>
+<HEAD><TITLE> EJ4B – Tabla Multiplicar</TITLE></HEAD>
+<BODY>
+<?php
+$num="8";
+for($i=1;$i<11;$i++){
+    echo $num."x".$i."=".($num*$i);
+    echo "<br/>";
+}
+?>
+</BODY>
+</HTML>
