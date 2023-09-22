605f7bcd5a4fc298f7eecfc68b3a0de4eb06c537
Franyxy <117320622+Franyxy@users.noreply.github.com>
2023-09-22 10:24:32 +0200

    Create BuclesEj06.php

---
+++ BuclesEj06.php
@@ -0,0 +1,13 @@
+<HTML>
+<HEAD><TITLE> EJ4B – Tabla Multiplicar</TITLE></HEAD>
+<BODY>
+<?php
+$num="5";
+$fact="1";
+for($i=1;$i<=$num;$i++){
+    $fact=$fact*$i;
+}
+echo "El factorial de ".$num." es: ".$fact;
+?>
+</BODY>
+</HTML>
