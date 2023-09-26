9ec4e9427bfab10257712383fdf0f004be6ebe41
Franyxy <117320622+Franyxy@users.noreply.github.com>
2023-09-26 22:05:27 +0200

    Update ArrayEj07.php

--- Ejercicios1/Array/ArrayEj07.php
+++ Ejercicios1/Array/ArrayEj07.php
@@ -2,14 +2,31 @@
 <html>
 <body>
 	<?php
-        $age=array("Edu"=>"22", "Andres"=>"20", "Gonzalo"=>"18","Victor"=>"19","Calixto"=>"23");
-        foreach($age as $x=>$i){
+        $edad=array("Edu"=>"22", "Andres"=>"20", "Gonzalo"=>"18","Victor"=>"19","Calixto"=>"23");
+        foreach($edad as $x=>$i){
             echo "Nombre: " . $x . " || Edad: " . $i;
             echo "<br>";
         }
-        $aux=key($age,"Andres");
-        echo $aux;
-
+        echo "</br>";
+        $aux=next($edad);
+        echo "El puntero colocado en la segunda posicion tiene de valor: ".$aux;
+        echo "</br>";
+        $aux=next($edad);
+        echo "El puntero con una posicion más tiene de valor: ".$aux;
+        echo "</br>";
+        $aux=end($edad);
+        echo "El puntero en la última posición tiene de valor: ".$aux;
+        echo "</br>";echo "</br>";
+        asort($edad);
+        foreach($edad as $x=>$i){
+            echo "Nombre: " . $x . " || Edad: " . $i;
+            echo "<br>";
+        }
+        echo "</br>";
+        echo "La primera posición del array tiene como valor: ".reset($edad);
+        echo "</br>";
+        echo "La ultima posición del array tiene como valor: ".end($edad);
+        echo "</br>";
     ?>
 
 </body>
