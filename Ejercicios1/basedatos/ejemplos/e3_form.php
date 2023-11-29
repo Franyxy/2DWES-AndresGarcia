c8670f3e072b996a056b791bc370dda89f30420f
Franyxy <117320622+Franyxy@users.noreply.github.com>
2023-11-29 12:49:27 +0100

    Add files via upload

---
+++ Ejercicios1/basedatos/ejemplos/e3_form.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!--
+        En el siguiente ejercicio, mostraremos por pantalla los datos de todos los empleados  que trabajan en un departamente seleccionado con anterioridad
+    -->
+    <h1>Consulta Empleados</h1>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "rootroot";
+            $dbname = "empleadosnn";
+            $conn1 = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            $conn1->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt1 = $conn1->prepare("SELECT nombre_dpto from departamento;");
+            $stmt1->execute();
+            $arrayNomdto=$stmt1->FetchAll(PDO::FETCH_COLUMN);
+            echo '<label for="nom_dto">Elige un departamento</label>';
+            echo '<select name="nom_dto" id="nom_dto">';
+            foreach($arrayNomdto as $x){
+                echo '<option value='.$x.'>'.$x.'</option>';
+            }
+            echo '</select><br><br>';
+        ?>
+        <input type="submit">
+        <input type="reset">
+    </form>
+    <?php
+        if($_SERVER["REQUEST_METHOD"]=="POST"){
+            $servername = "localhost";
+            $username = "root";
+            $password = "rootroot";
+            $dbname = "empleadosnn";
+
+            $valueOption=test_input($_POST['nom_dto']);
+            
+            try {
+                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                // set the PDO error mode to exception
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // prepare sql and bind parameters
+                $stmt = $conn->prepare("SELECT  empleado.dni,nombre_emple,salario,fecha_nac from empleado inner join emple_dpto on empleado.dni=emple_dpto.dni INNER JOIN departamento ON departamento.cod_dpto = emple_dpto.cod_dpto WHERE nombre_dpto='$valueOption'");
+                $stmt->execute();
+                $arrayDatosEmpleado=$stmt->FetchAll(PDO::FETCH_ASSOC);
+                echo "<br><table border='1px' style='border-collapse: collapse; text-align:center;'>";
+                echo "<tr><th>DNI</th><th>Nombre Empleado</th><th>Salario</th><th>Fecha Nacimiento</th></tr>";
+                foreach($arrayDatosEmpleado as $empleado){
+                    echo "<tr>";
+                    foreach($empleado as $dato => $valor){
+                        echo "<td>$valor</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo "</table>";
+
+                }
+            catch(PDOException $e)
+                {
+                echo "Error: " . $e->getMessage();
+                }
+            $conn = null;
+        }
+
+        function test_input($data) {
+            $data = trim($data);
+            $data = stripslashes($data);
+            $data = htmlspecialchars($data);
+            return $data;
+        }
+    ?>
+</body>
+</body>
+</html>
