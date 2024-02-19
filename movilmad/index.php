55aba17a7b2faa12528fc56cd3b6be00670a0f51
Franyxy <117320622+Franyxy@users.noreply.github.com>
2024-02-19 13:03:25 +0100

    Delete movilmad directory

--- movilmad/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if(!isset($_SESSION['email'])){
-        header('Location:./views/movlogin.php');
-    }else{
-        header('Location:./views/movwelcome.php');
-    }
-?>
