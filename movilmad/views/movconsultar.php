b1f99d2767ec3816337ee0731b7832bc300693a7
Franyxy <117320622+Franyxy@users.noreply.github.com>
2024-02-19 13:04:04 +0100

    Add files via upload

---
+++ movilmad/views/movconsultar.php
@@ -0,0 +1,50 @@
+﻿<?php
+    session_start();
+    if(!isset($_SESSION['email'])){
+        header('Location:./movlogin.php');
+    }
+?>
+<html>
+   
+ <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <title>Bienvenido a MovilMAD</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+  </head>
+   
+ <body>
+    <h1>Servicio de ALQUILER DE E-CARS</h1> 
+
+    <div class="container ">
+        <!--Aplicacion-->
+		<div class="card border-success mb-3" style="max-width: 30rem;">
+		<div class="card-header">Menú Usuario - CONSULTA ALQUILERES </div>
+		<div class="card-body">
+	  
+	  	
+	   
+
+	<!-- INICIO DEL FORMULARIO -->
+	<form action="" method="post">
+				
+		<B>Bienvenido/a: </B><?php echo $_SESSION['nombre'];?><BR><BR>
+		<B>Identificador Cliente: </B><?php echo $_SESSION['identificador'];?><BR><BR>
+		     
+			 Fecha Desde: <input type='date' name='fechadesde' value='' size=10 placeholder="fechadesde" class="form-control">
+			 Fecha Hasta: <input type='date' name='fechahasta' value='' size=10 placeholder="fechahasta" class="form-control"><br><br>
+				
+		<div>
+			<input type="submit" value="Consultar" name="Volver" class="btn btn-warning disabled">
+		
+			<input type="submit" value="Volver" name="Volver" class="btn btn-warning disabled">
+		
+		</div>		
+	</form>
+	<!-- FIN DEL FORMULARIO -->
+    <a href = "">Cerrar Sesion</a>
+
+  </body>
+   
+</html>
